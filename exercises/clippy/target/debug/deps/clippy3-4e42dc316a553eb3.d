61e0985a89363f7bbdfb052f0805f49023c7b860
Deusiratus <siran@Goten>
2023-08-21 20:06:02 -0500

    Initial commit

---
+++ exercises/clippy/target/debug/deps/clippy3-4e42dc316a553eb3.d
@@ -0,0 +1,8 @@
+/home/siran/repos/rust/rust_learning/rustlings/exercises/clippy/target/debug/deps/clippy3-4e42dc316a553eb3.rmeta: clippy3.rs Cargo.toml
+
+/home/siran/repos/rust/rust_learning/rustlings/exercises/clippy/target/debug/deps/clippy3-4e42dc316a553eb3.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
